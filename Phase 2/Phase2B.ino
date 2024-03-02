6fc60216ac8f275625f26562a9780dcfde940e98
eddyabban2 <128445528+eddyabban2@users.noreply.github.com>
2024-03-02 12:43:08 -0500

    Update Phase2B.ino
    Updated the code to use digitalWrites and reads properly and I updated the pinouts

--- Phase 2/Phase2B.ino
+++ Phase 2/Phase2B.ino
@@ -1,10 +1,10 @@
 // pin setup
-#define leftFor A0
-#define leftRev A1
-#define leftEnable 4
-#define rightFor A2
-#define rightRev A3
-#define rightEnable 5
+#define leftFor 2
+#define leftRev 3
+#define leftEnable 5
+#define rightFor 7
+#define rightRev 8
+#define rightEnable 9 
 
 
 
@@ -17,53 +17,53 @@ void setup() {
   pinMode(leftEnable, OUTPUT);
   pinMode(rightEnable, OUTPUT);
 
-  digitalWrite(leftEnable, HIGH);
-  digitalWrite(rightEnable, HIGH);
+  analogWrite(leftEnable, 255);
+  analogWrite(rightEnable, 255);
 }
 
 void loop() {
-  forward(1000);
-  rev(1000);
-  right(180);
-  left(180);
+  Serial.println("eddy was here");
+//  forward(1000);
+//  rev(1000);
+//  right(180);
+//  left(180);
 
 }
 
 void forward(int duration) // duration in sec
 {
-  analogWrite(leftRev, 0);
-  analogWrite(rightRev, 0);
-  analogWrite(leftFor, 255);
-  analogWrite(rightFor, 255);
+  digitalWrite(leftRev, LOW);
+  digitalWrite(rightRev, LOW);
+  digitalWrite(leftFor, HIGH);
+  digitalWrite(rightFor, HIGH);
   delay(duration);
 }
 
 void rev(int duration) // duration in sec
 {
-  analogWrite(leftRev, 255);
-  analogWrite(rightRev, 255);
-  analogWrite(leftFor, 0);
-  analogWrite(rightFor, 0);
+  digitalWrite(leftRev, HIGH);
+  digitalWrite(rightRev, HIGH);
+  digitalWrite(leftFor, LOW);
+  digitalWrite(rightFor, LOW);
   delay(duration);
 }
 
 void right(int angle) // duration in sec
 {
-  analogWrite(leftRev, 0);
-  analogWrite(rightRev, 255);
-  analogWrite(leftFor, 255);
-  analogWrite(rightFor, 0);
+  digitalWrite(leftRev, LOW);
+  digitalWrite(rightRev, HIGH);
+  digitalWrite(leftFor, HIGH);
+  digitalWrite(rightFor, LOW);
   int delay_time = (turn_const/360)*angle;
   delay(delay_time);
 }
 
 void left(int angle)
 {
-  analogWrite(leftRev, 255);
-  analogWrite(rightRev, 0);
-  analogWrite(leftFor, 0);
-  analogWrite(rightFor, 255);
+  digitalWrite(leftRev, HIGH);
+  digitalWrite(rightRev, LOW);
+  digitalWrite(leftFor, LOW);
+  digitalWrite(rightFor, HIGH);
   int delay_time = (turn_const/360)*angle;
   delay(delay_time);
 }
-
