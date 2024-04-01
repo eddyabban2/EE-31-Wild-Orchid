1c2deb1d7590a57e0fad2b933503b857f108c7a3
eddyabban2 <128445528+eddyabban2@users.noreply.github.com>
2024-04-01 19:47:36 -0400

    Create 3a.ino

---
+++ phase_3a/3a.ino
@@ -0,0 +1,17 @@
+#define redPin 10 
+#define bluePin 11
+
+
+
+
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(rightFor, INTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
