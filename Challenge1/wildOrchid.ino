6ed2851fb556e887fac35a3362c5926326ff7fe5
eddyabban2 <128445528+eddyabban2@users.noreply.github.com>
2024-05-01 18:56:33 -0400

    Update wildOrchid.ino

--- Challenge1/wildOrchid.ino
+++ Challenge1/wildOrchid.ino
@@ -1,27 +1,27 @@
 #include <SPI.h>
 #include <WiFiNINA.h>
 
-#define redPin 10 // color detection blue and red
+#define redPin 10  // color detection blue and red
 #define bluePin 11
 #define batteryIndicatorLED 12
 #define dayIndicatorLED 13
 #define IRDector A3
 
-#define ambientLightPin A2 // detectors 
+#define ambientLightPin A2  // detectors
 #define colorPin A1
 #define batteryPin A0
 
-#define leftFor 2 // motor control
+#define leftFor 2  // motor control
 #define leftRev 3
 #define rightFor 7
 #define rightRev 8
 
-#define blueSig 0 // indicator LEDS 
+#define blueSig 0  // indicator LEDS
 #define redSig 1
 #define greenSig 4
 #define yellowSig 6
 
-#define brakeLights 5 // Lights
+#define brakeLights 5  // Lights
 #define headLights 9
 #define leftTurnSignal A4
 #define rightTurnSignal A5
@@ -38,7 +38,7 @@ String our_id = "F392FC86D8D7";
 String companion_team_id = "F79721857DC5";
 
 String getRoute = "GET /" + our_id + "/" + companion_team_id + " HTTP/1.1";
-String postRoute = "POST /" + companion_team_id + "/" + our_id  + " HTTP/1.1";
+String postRoute = "POST /" + companion_team_id + "/" + our_id + " HTTP/1.1";
 
 WiFiClient client;
 
@@ -67,17 +67,15 @@ void setup() {
   pinMode(redSig, OUTPUT);
   pinMode(greenSig, OUTPUT);
   pinMode(yellowSig, OUTPUT);
+  digitalWrite(yellowSig, HIGH);
   setupWifi();
+  digitalWrite(yellowSig, LOW);
 }
 void loop() {
   // botOne();
+  determine_collision();
   // colorTest();
-  determine_color();
-  // while(not determine_collision()) {
-  //   find(red);
-  //   forward(50);
-  //   delay(50);
-  // }
+  // determine_color();
   // String newRoute = "GET /" + our_id + "/" + our_id + " HTTP/1.1";
   // String message = GET(newRoute);
   // Serial.println(message);
@@ -92,36 +90,34 @@ void botOne() {
     forward(50);
   }
   stop(1000);
-  pivot_left(180);
-  forward(100);
+  pivot_left(750);
   // Turn 180 degrees
   // Move forward until it finds red
   while (determine_color() != red) {
-    forward(50);
+    forward(100);
   }
   // Turn on the red led
   digitalWrite(redSig, HIGH);
-  stop(1000);
+  stop(10);
 
   // Use light based communication to tell the other bot that it can go
   // add light based communication here
-   // flash headlights to fake communication
-  POST(postRoute, "you_can_go");
-  for(int i = 0; i < 2; i++)
-  {
+  // flash headlights to fake communication
+  POST(postRoute, "you_can_go_from_2");
+  for (int i = 0; i < 2; i++) {
     digitalWrite(headLights, HIGH);
     delay(250);
     digitalWrite(headLights, LOW);
     delay(250);
   }
   // Get a light based message back from bot 2, flash headlights and brake lights, beep horn twice
-  //maybe use wifi for this so we sont worry about the 
+  //maybe use wifi for this so we don't worry about the light sensing filter thingy
   // add light based communication here
-  while(GET(getRoute) != "you_can_go") {
+  while (GET(getRoute) != "you_can_go_from_2") {
     stop(500);
   }
-  for(int i = 0; i < 2; i++)
-  {
+  for (int i = 0; i < 2; i++) {
+    //also add horn here
     digitalWrite(headLights, HIGH);
     digitalWrite(brakeLights, HIGH);
     delay(250);
@@ -130,12 +126,11 @@ void botOne() {
     delay(250);
   }
 
-
-
+  forward(200);
+  pivot_left(300);
   // Go on red until wall collision or end of red
   while (not determine_collision()) {
     find(red);
-    forward(50);
   }
   digitalWrite(redSig, LOW);
   stop(1000);
@@ -144,16 +139,18 @@ void botOne() {
   // Blink red led
   blinkRed(2);
   // do some magic to get the second signal
-
+  // while (GET(getRoute) != "blink_red_from_2") {
+  //   stop(500);
+  // }
   // When you receive bot 2’s signal blink red led 3 times
   blinkRed(3);
   // Turn on green led
   digitalWrite(greenSig, HIGH);
-    // Back up
-    reverse(100);
-    stop(1000);
+  // Back up
+  reverse(100);
+  stop(1000);
   // Make 90 degree left turn (exactly 90)
-  pivot_left(90);
+  pivot_left(300);
   stop(1000);
   // Go until detect yellow
   while (determine_color() != yellow and determine_color() != yellow and determine_color() != yellow) {
@@ -161,31 +158,126 @@ void botOne() {
   }
   stop(1000);
   // Beep the horn twice
-  // figure out how to beep the horn 
+  // figure out how to beep the horn
 
-  pivot_left(90);
+ 
+  pivot_left(300);
   stop(1000);
   // Follow yellow path until the end
-  while (determine_color() == yellow and determine_color() == yellow and determine_color() == yellow) {
-    forward(50);
+  while (not determine_collision()) {
+    find(yellow);
   }
   stop(1000);
 
-  pivot_left(90);
+  pivot_left(300);
   stop(1000);
   // Turn left and go to end
   forward(300);
   stop(1000);
   // Signal to bot two that you have reached the end
+  POST(postRoute, "bot_one_done!");
+   for (int i = 0; i < 2; i++) {
+    digitalWrite(headLights, HIGH);
+    delay(250);
+    digitalWrite(headLights, LOW);
+    delay(250);
+  }
+  while(true);
 }
 
 void botTwo() {
+  //get message from bot one
+  // while (GET(getRoute) != "you_can_go_from_1") {
+  //   stop(500);
+  // }
+  //flash headlights and brakelights, honk horn
+   for (int i = 0; i < 2; i++) {
+    //also add horn here
+    digitalWrite(headLights, HIGH);
+    digitalWrite(brakeLights, HIGH);
+    delay(250);
+    digitalWrite(headLights, LOW);
+    digitalWrite(brakeLights, LOW);
+    delay(250);
+  }
+  delay(1000);
+  POST(postRoute, "you_can_go_from_2");
+  for (int i = 0; i < 2; i++) {
+    digitalWrite(headLights, HIGH);
+    delay(250);
+    digitalWrite(headLights, LOW);
+    delay(250);
+  }
+  delay(5000);
+
+  while (not determine_collision()) {
+    forward(50);
+  }
+  stop(1000);
+  pivot_left(750);
+  // Turn 180 degrees
+  // Move forward until it finds red
+  while (determine_color() != blue) {
+    forward(100);
+  }
+  // Turn on the red led
+  digitalWrite(blueSig, HIGH);
+  stop(10);
+
+  pivot_right(300);
+  // Go on red until wall collision or end of red
+  while (not determine_collision()) {
+    find(blue);
+  }
+  digitalWrite(blueSig, LOW);
+  digitalWrite(greenSig, HIGH);
+  while (GET(getRoute) != "bot_one_done") {
+    stop(500);
+  }
+
+
+
+  // Blink red led
+  blinkGreen(2);
+  // Back up
+  reverse(100);
+  stop(1000);
+  // Make 90 degree left turn (exactly 90)
+  pivot_right(90);
+  stop(1000);
+  // while(GET(getRoute) != "bot_one_done") {
+  //   stop(500)
+  // }
+  // Go until detect yellow
+  while (determine_color() != yellow and determine_color() != yellow and determine_color() != yellow) {
+    forward(50);
+  }
+  stop(1000);
+  // Beep the horn twice
+  // figure out how to beep the horn
+
+ 
+  pivot_right(90);
+  stop(1000);
+  // Follow yellow path until the end
+  while (not determine_collision()) {
+    find(yellow);
+  }
+  digitalWrite(yellowSig, HIGH);
+  stop(1000);
+
+  pivot_right(90);
+  stop(1000);
+  // Turn left and go to end
+  forward(300);
+  stop(1000);
+ 
+  while(true);
+
 }
 
-void colorTest()
-{
-  while(true)
-  {
+void colorTest() {
+  while (true) {
     digitalWrite(leftFor, HIGH);
     digitalWrite(rightFor, HIGH);
     determine_color();
@@ -196,6 +288,15 @@ void blinkRed(int times) {
     digitalWrite(redSig, HIGH);
     delay(200);
     digitalWrite(redSig, LOW);
+    delay(200);
+  }
+}
+
+void blinkGreen(int times) {
+  for (int i = 0; i < times; i++) {
+    digitalWrite(greenSig, HIGH);
+    delay(200);
+    digitalWrite(greenSig, LOW);
     delay(200);
   }
 }
@@ -215,7 +316,7 @@ bool determine_collision() {
   int reading = analogRead(IRDector);
   Serial.print("reading:");
   Serial.println(reading);
-  bool output = reading > 250;
+  bool output = reading > 350;
   return output;
 }
 
@@ -225,25 +326,25 @@ Color determine_color() {
   int red_reading = take_reading(LOW, HIGH) + brightness;
   int both_reading = take_reading(HIGH, HIGH) + brightness;
   int diff = red_reading - blue_reading;
-  int acceptable_range = 10;
+  int acceptable_range = 40;
   // String currPR = "POST /" + our_id + "/" + our_id  + " HTTP/1.1";
   // String readings = "blue:" + String(blue_reading) + "red:" + String(red_reading) + "both:"+ String(both_reading);
   // POST(currPR, readings);
-  int blue_blue_read = 25;
-  int blue_red_read = 36;
-  int blue_both_read = 64;
+  int blue_blue_read = 873;
+  int blue_red_read = 898;
+  int blue_both_read = 779;
 
-  int red_blue_read = 14;
-  int red_red_read = 68;
-  int red_both_read = 86;
+  int red_blue_read = 910;
+  int red_red_read = 700;
+  int red_both_read = 645;
 
-  int yellow_blue_read = 43;
-  int yellow_red_read = 96;
-  int yellow_both_read = 142;
+  int yellow_blue_read = 650;
+  int yellow_red_read = 588;
+  int yellow_both_read = 465;
 
-  int black_blue_read = 9;
-  int black_red_read = 29;
-  int black_both_read = 41;
+  int black_blue_read = 937;
+  int black_red_read = 917;
+  int black_both_read = 847;
 
   Serial.print("Blue, Red, Both Readings: ");
   Serial.print(blue_reading);
@@ -256,14 +357,12 @@ Color determine_color() {
   if (abs(red_reading - blue_red_read) <= acceptable_range) {
     if (abs(blue_reading - blue_blue_read) <= acceptable_range) {
       if (abs(both_reading - blue_both_read) <= acceptable_range) {
-        if (diff < 15) {
-          Serial.println("blue");
-          digitalWrite(redSig, LOW);
-          digitalWrite(blueSig, HIGH);
-          digitalWrite(yellowSig, LOW);
-          digitalWrite(greenSig, LOW);
-          return blue;
-        }
+        Serial.println("blue");
+        digitalWrite(redSig, LOW);
+        digitalWrite(blueSig, HIGH);
+        digitalWrite(yellowSig, LOW);
+        digitalWrite(greenSig, LOW);
+        return blue;
       }
     }
   }
@@ -271,9 +370,9 @@ Color determine_color() {
     if (abs(blue_reading - red_blue_read) <= acceptable_range) {
       if (abs(both_reading - red_both_read) <= acceptable_range) {
         digitalWrite(redSig, HIGH);
-          digitalWrite(blueSig, LOW);
-          digitalWrite(yellowSig, LOW);
-          digitalWrite(greenSig, LOW);
+        digitalWrite(blueSig, LOW);
+        digitalWrite(yellowSig, LOW);
+        digitalWrite(greenSig, LOW);
         Serial.println("red");
         return red;
       }
@@ -285,9 +384,9 @@ Color determine_color() {
       if (abs(both_reading - yellow_both_read) <= acceptable_range) {
         Serial.println("yellow");
         digitalWrite(redSig, LOW);
-          digitalWrite(blueSig, LOW);
-          digitalWrite(yellowSig, HIGH);
-          digitalWrite(greenSig, LOW);
+        digitalWrite(blueSig, LOW);
+        digitalWrite(yellowSig, HIGH);
+        digitalWrite(greenSig, LOW);
         digitalWrite(redSig, LOW);
         return yellow;
       }
@@ -298,14 +397,12 @@ Color determine_color() {
   if (abs(red_reading - black_red_read) <= acceptable_range) {
     if (abs(blue_reading - black_blue_read) <= acceptable_range) {
       if (abs(both_reading - black_both_read) <= acceptable_range) {
-        if (diff > 10) {
-          Serial.println("black");
-          digitalWrite(redSig, LOW);
-          digitalWrite(blueSig, LOW);
-          digitalWrite(yellowSig, LOW);
-          digitalWrite(greenSig, HIGH);
-          return black;
-        }
+        Serial.println("black");
+        digitalWrite(redSig, LOW);
+        digitalWrite(blueSig, LOW);
+        digitalWrite(yellowSig, LOW);
+        digitalWrite(greenSig, HIGH);
+        return black;
       }
     }
   }
@@ -314,16 +411,17 @@ Color determine_color() {
 
 int take_reading(bool blue, bool red) {
   digitalWrite(bluePin, blue);
+  int num_readings = 10;
   digitalWrite(redPin, red);
-  delay(20);
+  delay(10);
   float sum = 0;
-  for (int i = 0; i < 30; i++) {
+  for (int i = 0; i < num_readings; i++) {
     sum += analogRead(colorPin);
   }
-  delay(20);
+  delay(10);
   digitalWrite(bluePin, LOW);
   digitalWrite(redPin, LOW);
-  return sum / 30;
+  return sum / num_readings;
 }
 
 
@@ -355,8 +453,7 @@ void pivot_right(int angle)  // duration in sec
   digitalWrite(rightRev, HIGH);
   digitalWrite(leftFor, HIGH);
   digitalWrite(rightFor, LOW);
-  double delay_time = (turn_const / 360.0) * angle;
-  delay((int)delay_time - 100);
+  delay(angle);
   digitalWrite(rightRev, LOW);
   digitalWrite(leftFor, LOW);
 }
@@ -366,8 +463,7 @@ void pivot_left(int angle) {
   digitalWrite(rightRev, LOW);
   digitalWrite(leftFor, LOW);
   digitalWrite(rightFor, HIGH);
-  double delay_time = (turn_const / 360.0) * angle;
-  delay((int)delay_time);
+  delay(angle);
   digitalWrite(leftRev, LOW);
   digitalWrite(rightFor, LOW);
 }
@@ -432,20 +528,23 @@ void printWifiStatus() {
   Serial.println(" dBm");
 }
 
-void stop(int time){ //time
-  digitalWrite(brakeLights,HIGH);
-  while(time>0) {
-    forward(1);
-    reverse(1);
-    time = time - 2;
-  }
-  digitalWrite(brakeLights,LOW);
+void stop(int time) {  //time
+  digitalWrite(brakeLights, HIGH);
+  digitalWrite(leftRev, HIGH);
+  digitalWrite(rightRev, HIGH);
+  digitalWrite(leftFor, HIGH);
+  digitalWrite(rightFor, HIGH);
+  delay(50);
+  digitalWrite(leftRev, LOW);
+  digitalWrite(rightRev, LOW);
+  digitalWrite(leftFor, LOW);
+  digitalWrite(rightFor, LOW);
+  digitalWrite(brakeLights, LOW);
 }
 
 void POST(String theRoute, String message) {
   message = "message=" + message;
-  if(client.connect(server, portNumber))
-  {
+  if (client.connect(server, portNumber)) {
     client.println(theRoute);
     client.print("Host: ");
     client.println(server);
@@ -481,23 +580,19 @@ String GET(String theRoute) {
   }
 }
 
-String process_message(String message)
-{
+String process_message(String message) {
   // Serial.println(message);
   int index = 0;
   int count_new_line = 0;
-  while(index < message.length() and count_new_line < 3)
-  {
-    if(message[index] == '&')
-    {
+  while (index < message.length() and count_new_line < 3) {
+    if (message[index] == '&') {
       count_new_line += 1;
     }
     index++;
   }
   index += 8;
   String output_string = "";
-  while(index < message.length())
-  {
+  while (index < message.length()) {
     output_string += message[index];
     index++;
   }
@@ -505,16 +600,13 @@ String process_message(String message)
 }
 
 void find(Color c) {
-  if(determine_color() == c or determine_color() == c or determine_color() == c)
-    {
-      //forward(50);
-      pivot_right(10);
-    }
-    else 
-    {
-      while(determine_color() != c) {
-        pivot_left(10);
-      }
-      
-    }
+  Serial.println("got into find");
+  if (determine_color() == c or determine_color() == c or determine_color() == c) {
+    Serial.println("color is correct pvioting right");
+    forward(500);
+    pivot_right(50);
+  } else {
+    Serial.println("color is incorrect pivoting left");
+    pivot_left(50);
+  }
 }
